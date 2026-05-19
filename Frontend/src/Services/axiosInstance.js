476e849684e3f62c3599bd2a595bced9de964ab6
Vardhan-2603 <pubg26272828@gmail.com>
2026-05-19 10:09:46 +0530

    Final Commit

--- Frontend/src/Services/axiosInstance.js
+++ Frontend/src/Services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://expensive-tracker-with-ai-insights.onrender.com',
+  baseURL: 'https://expensive-tracker-with-ai-insights.onrender.com',
   withCredentials: true,
 });
 
